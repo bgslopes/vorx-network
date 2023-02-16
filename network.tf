aa42886d67a0f5744a7e630bffd47b72703df596
root <root@ip-172-31-26-80.ec2.internal>
2023-02-16 00:04:04 +0000

    Add: Creat subnets priv and pub

--- network.tf
+++ network.tf
@@ -6,6 +6,29 @@ resource "aws_vpc" "vorx-vpc-prod" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Privat-subnet-1a"
+  }
+}
+
+
+
+
+
+
 
 ## OUTPUTS DO NOSSO TF ##
 output "vpc_vorx_prod_id"{
@@ -15,5 +38,21 @@ value = aws_vpc.vorx-vpc-prod.id
 output "vpc_vorx_prod_arn" {
 value = aws_vpc.vorx-vpc-prod.arn
 }
+
+output "vorx_prod_subnet_pub-1a" {
+value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "vorx_prod_subnet_priv-1a" {
+value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
+
+
+
+
+
+
+
 
 
